fb22449e341aa0a86956be9a3adf21881b6c69fb
Sambarilov <vitorhp2@gmail.com>
2025-02-14 11:48:54 -0300

    added number of visible people in a queue

--- array/traversing_from_the_right.rb
+++ array/traversing_from_the_right.rb
@@ -23,5 +23,35 @@ def daily_temperatures(temps)
   answer
 end
 
-puts daily_temperatures([30, 20, 30, 90]).inspect
-puts daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]).inspect
+# puts daily_temperatures([30, 20, 30, 90]).inspect
+# puts daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]).inspect
+
+
+def visible_people_in_a_line(heights)
+  answer = Array.new(heights.size) { 0 }
+  head = heights.size - 2
+  tail = heights.size - 1
+  current_talest = heights[tail]
+
+  while head >= 0
+    answer[head] = 1
+
+    while tail < heights.size && heights[head] > heights[tail]
+      if heights[tail] > current_talest
+        answer[head] += 1 
+        current_talest = heights[tail]
+      end
+
+      tail += 1
+    end
+
+    head -= 1
+    tail = head + 1
+    current_talest = 0
+  end
+
+  answer
+end
+
+puts visible_people_in_a_line([10, 6, 5, 11]).inspect
+puts visible_people_in_a_line([10,6,8,5,11,9]).inspect
