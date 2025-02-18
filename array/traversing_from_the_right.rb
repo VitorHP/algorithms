cfb4ad9830f27951280d8a77a5338b1883a34e6c
Sambarilov <vitorhp2@gmail.com>
2025-02-18 08:01:30 -0300

    Added Visible people in a line problem

--- array/traversing_from_the_right.rb
+++ array/traversing_from_the_right.rb
@@ -9,9 +9,7 @@ def daily_temperatures(temps)
     if temps[tail] > temps[head]
       answer[head] = tail - head
     else
-      while tail < temps.size && temps[tail] <= temps[head]
-        tail += 1
-      end
+      tail += 1 while tail < temps.size && temps[tail] <= temps[head]
 
       answer[head] = tail < temps.size ? tail - head : 0
     end
@@ -26,32 +24,25 @@ end
 # puts daily_temperatures([30, 20, 30, 90]).inspect
 # puts daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]).inspect
 
-
 def visible_people_in_a_line(heights)
-  answer = Array.new(heights.size) { 0 }
   head = heights.size - 2
-  tail = heights.size - 1
-  current_talest = heights[tail]
+  visible_people = [heights.last]
+  answer = Array.new(heights.size) { 0 }
 
   while head >= 0
-    answer[head] = 1
+    (visible_people.size - 1).downto(0) do |i|
+      answer[head] += 1
 
-    while tail < heights.size && heights[head] > heights[tail]
-      if heights[tail] > current_talest
-        answer[head] += 1 
-        current_talest = heights[tail]
-      end
-
-      tail += 1
+      break if visible_people[i] >= heights[head]
     end
 
+    visible_people.pop while !visible_people.empty? && visible_people.last <= heights[head]
+    visible_people.push heights[head]
+
     head -= 1
-    tail = head + 1
-    current_talest = 0
   end
 
   answer
 end
 
-puts visible_people_in_a_line([10, 6, 5, 11]).inspect
-puts visible_people_in_a_line([10,6,8,5,11,9]).inspect
+puts visible_people_in_a_line([10, 6, 8, 5, 11, 9]).inspect
